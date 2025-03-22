991f38c4013a1c25c735777f080a2b7ce6fa235f
Orly Antonio Carvalho Neto <orlyprogramador2009@gmail.com>
2025-03-22 13:46:33 -0300

    new method string_is_number() added and say_hello() is more sophisticated now. Also, workflow problem seems fixed.

--- quickMethods/main.py
+++ quickMethods/main.py
@@ -1,12 +1,26 @@
+import getpass
+
 class QuickMethods:
     def __init__(self):
         pass
-
-    def say_hello():
-        print("Hello!")
     
-    def say_bye():
-        print("Goodbye!")
+    def __repr__(self):
+        return "QuickMethods()"
+
+    def say_hello(self) -> None:
+        try:
+            print(f"Hello, {getpass.getuser()}!")
+        except:
+            print("Hello! Hmm... I wasn't able to guess your name... Hello John Doe!")
+    
+    def string_is_number(self, string: str) -> bool:
+        try:
+            complex(string)
+            return True
+        except ValueError:
+            return False
+    
+    
     
 class Number():
     def __init__(self, x: int | float | complex):
@@ -89,3 +103,12 @@ class Number():
         if type(self.x_number) == float:
             number_is_float = True
         return number_is_float
+
+if __name__ == "__main__":
+    qm = QuickMethods()
+    print(repr(qm), end="\n\n")
+    
+    qm.say_hello()
+    print(qm.string_is_number("7j"))
+    
+    first_value = Number(12)
