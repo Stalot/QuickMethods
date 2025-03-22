f6da9e485b4201d1517e999d457f231fd42d5fea
Orly Antonio Carvalho Neto <orlyprogramador2009@gmail.com>
2025-03-21 23:27:25 -0300

    Trying to fix long_description() FileNotFound error in GitHub workflow. (x9)

--- setup.py
+++ setup.py
@@ -2,33 +2,27 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 def long_description() -> str:
+    base_dir: Path = Path(__file__).parent
     long_description: str = ''
+    
     print("Extracting content from README.md...")
-    try:
-        base_dir: Path = Path(__file__).parent
-        filepath: Path = base_dir / 'README.md'
-        print("README PATH:", filepath)
-        with open(filepath, 'r', encoding='utf-8') as file:
-            long_description += file.read()
-        print("README.md done.")
-    except Exception as e:
-        print(f"Failed to read README.md content: {e}")
+    filepath: Path = base_dir / 'README.md'
+    with open(filepath, 'r', encoding='utf-8') as file:
+        long_description += file.read()
+    print("README.md done.")
+
     print("Extracting content from CHANGELOG.md...")
-    try:
-        base_dir: Path = Path(__file__).parent
-        filepath: Path = base_dir / 'CHANGELOG.md'
-        print("CHANGELOG PATH:", filepath)
-        with open(filepath, 'r', encoding='utf-8') as file:
-            long_description += f'\n\n{file.read()}'
-        print("CHANGELOG.md done.")
-    except Exception as e:
-        print(f"Failed to read CHANGELOG.md content: {e}")
+    filepath: Path = base_dir / 'CHANGELOG.md'
+    with open(filepath, 'r', encoding='utf-8') as file:
+        long_description += f'\n\n{file.read()}'
+    print("CHANGELOG.md done.")
+        
     print("Long description built successfully.")
     return long_description
 
 setup(
     name='quickMethods',
-    version='0.0.3',
+    version='0.0.4',
     description='A simple library to make your life faster with quick methods!',
     long_description=long_description(),
     long_description_content_type='text/markdown',
@@ -36,4 +30,5 @@ setup(
     author_email='orly2carvalhoneto@gmail.com',
     license='MIT License',
     keywords=['useful'],
-    packages=find_packages())
+    packages=find_packages(),
+    include_package_data=True)
